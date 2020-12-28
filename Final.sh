4109d1e849c83dfda8ae7e41dea2b66c6fa691bf
wiegehtki.de <58086193+wiegehtki@users.noreply.github.com>
2020-12-28 14:37:16 +0100

    Update Final.sh

--- Final.sh
+++ Final.sh
@@ -2,6 +2,10 @@
 
 # Es wird empfohlen root als Benutzer zu verwenden 
 Benutzer="root"
+CUDA_Version=11.2.0
+CUDA_Script=cuda_11.2.0_460.27.04_linux.run
+CUDA_Pfad=/usr/local/cuda-11.2
+
 
 
 if [ "$(whoami)" != $Benutzer ]; then
@@ -9,30 +13,34 @@ if [ "$(whoami)" != $Benutzer ]; then
         exit 255
 fi
 
-echo $(date -u) "Test auf bestehende Installation.log"
-                 test -f ~/Installation.log && rm ~/Installation.log
+echo $(date -u) "Test auf bestehende FinalInstall.log"
+                 test -f ~/FinalInstall.log && rm ~/FinalInstall.log
 
-echo $(date -u) "Installation.log anlegen"
-                 touch ~/Installation.log
+echo $(date -u) "FinalInstall.log anlegen"
+                 touch ~/FinalInstall.log
 				 
 ####################################################################################################################
 # Ubuntu 18.04 notwendig
 #
 #
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "04 von 30: CUDA runterladen und samt Grafiktreiber installieren"  | tee -a  ~/Installation.log
-                wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda_11.2.0_460.27.04_linux.run
-                sudo sh cuda_11.2.0_460.27.04_linux.run
-echo $(date -u) "04.1 von 30: CUDA Umgebung setzen"  | tee -a  ~/Installation.log
-                echo /usr/local/cuda-11.2/lib64 >>  /etc/ld.so.conf
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "01 von 30: CUDA runterladen und samt Grafiktreiber installieren"  | tee -a  ~/FinalInstall.log
+                wget https://developer.download.nvidia.com/compute/cuda/$CUDA_Version/local_installers/$CUDA_Script
+				chmod +x $CUDA_Script
+                ./$CUDA_Script --silent
+echo $(date -u) "01.1 von 30: Check auf installierten Treiber"  | tee -a  ~/FinalInstall.log
+				lshw -C display | tee -a  ~/FinalInstall.log
+				
+echo $(date -u) "01.2 von 30: CUDA Umgebung setzen"  | tee -a  ~/FinalInstall.log
+                echo $CUDA_Pfad/lib64 >>  /etc/ld.so.conf
                 ldconfig
-                echo 'export PATH=/usr/local/cuda-11.2/bin:$PATH' >> ~/.bashrc
+                echo 'export PATH='$CUDA_Pfad'/bin:$PATH' >> ~/.bashrc
                 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
                 echo 'cd ~' >> ~/.bashrc
                 source ~/.bashrc
 
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/Installation.log
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "03 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/FinalInstall.log
                 export DEBCONF_NONINTERACTIVE_SEEN="true"
                 export DEBIAN_FRONTEND="noninteractive"
                 export DISABLE_SSH="true"
@@ -60,15 +68,15 @@ echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installatio
                 export MULTI_PORT_START="10" 
                 export MULTI_PORT_END="0" 
 
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/Installation.log
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "04 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/FinalInstall.log
 
                 #mkdir /mnt/cache/appdata/Zoneminder
 
                 cd ~
-                git clone https://github.com/dlandon/zoneminder.master-docker.git
-                cp -r zoneminder.master-docker/init/. /etc/my_init.d/.                                
-                cp -r zoneminder.master-docker/defaults/. /root/.
+                #git clone https://github.com/dlandon/zoneminder.master-docker.git
+                cp -r zoneminder/init/. /etc/my_init.d/.                                
+                cp -r zoneminder/defaults/. /root/.
 
                 add-apt-repository -y ppa:iconnor/zoneminder-$ZM_VERS 
 	            LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php 
@@ -83,8 +91,8 @@ echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installatio
 	            apt-get -y install --no-install-recommends libvlc-dev libvlccore-dev vlc && \
 	            apt-get -y install zoneminder
 
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/Installation.log
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/FinalInstall.log
 
                 a2enmod proxy_fcgi setenvif	
                 a2enconf php7.4-fpm
@@ -103,8 +111,8 @@ echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installatio
                 perl -MCPAN -e "force install Net::MQTT::Simple" 
                 perl -MCPAN -e "force install Net::MQTT::Simple::Auth"
 
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/Installation.log
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/FinalInstall.log
 
                 mysql -uroot -D zm -e "DROP DATABASE zm"
                 cd /root 
@@ -120,8 +128,8 @@ echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installatio
                 mysql -sfu root < "mysql_defaults.sql" 
                 rm mysql_defaults.sql
 
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/Installation.log
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/FinalInstall.log
                 mv /root/zoneminder /etc/init.d/zoneminder
                 chmod +x /etc/init.d/zoneminder 
                 service mysql restart 
@@ -141,8 +149,8 @@ echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installatio
                 cp /etc/apache2/ports.conf /etc/apache2/ports.conf.default 
                 cp /etc/apache2/sites-enabled/default-ssl.conf /etc/apache2/sites-enabled/default-ssl.conf.default
 	
-echo $(date -u) "....................................................................................................................................." | tee -a  ~/Installation.log
-echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/Installation.log
+echo $(date -u) "....................................................................................................................................." | tee -a  ~/FinalInstall.log
+echo $(date -u) "05 von 30: Umgebungsvariablen für die Zoneminder - Installation setzen"  | tee -a  ~/FinalInstall.log
                 apt -y remove make
                 apt -y clean 
                 apt -y autoremove 
