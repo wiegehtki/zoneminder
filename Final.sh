923ecdb07105852cf07998e6e5d7b4663a1a895e
wiegehtki.de <58086193+wiegehtki@users.noreply.github.com>
2022-02-18 16:59:01 +0100

    Update Final.sh
    cuDNN Fix

--- Final.sh
+++ Final.sh
@@ -429,49 +429,52 @@ Logging "#######################################################################
         Logging "$infoStep1"
         tar -xf $CUDNN_VERSION
         
-        cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include 
-        cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64 
-        chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
+        mv ~/$CUDNN_VERSION /usr/local/cuda-$CUDA_VERSION/cudnn
+        chmod a+r /usr/local/cuda-$CUDA_VERSION/cudnn
+        
+        #cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda$CUDA_VERSION/include 
+        #cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda$CUDA_VERSION/lib64 
+      #  chmod a+r /usr/local/cuda$CUDA_VERSION/include/cudnn*.h /usr/local/cuda$CUDA_VERSION/lib64/libcudnn*
         
         #cp $CUDNN_DIRECTORY/include/cudnn*.h /usr/local/cuda/include
         #cp $CUDNN_DIRECTORY/lib64/libcudnn* /usr/local/cuda/lib64
         #chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
         
-        #cd /usr/local/cuda/lib64
-        #if [ -f libcudnn.so ];   then rm libcudnn.so;   fi 
-        #if [ -f libcudnn.so.8 ]; then rm libcudnn.so.8; fi 
-        #ln libcudnn.so.$cuDNN_MajorVersion libcudnn.so.8
-        #ln libcudnn.so.8 libcudnn.so
+     #   cd /usr/local/cuda/lib64
+     #   if [ -f libcudnn.so ];   then rm libcudnn.so;   fi 
+     #   if [ -f libcudnn.so.8 ]; then rm libcudnn.so.8; fi 
+     #   ln libcudnn.so.$cuDNN_MajorVersion libcudnn.so.8
+     #   ln libcudnn.so.8 libcudnn.so
         
-        #Logging "InstallcuDNN $infoStep2"
-        #cd  /usr/local/cuda-$CUDA_VERSION/targets/x86_64-linux/lib
-        #if [ -f libcudnn_adv_infer.so ];   then rm libcudnn_adv_infer.so;   fi 
-        #if [ -f libcudnn_ops_train.so ];   then rm libcudnn_ops_train.so;   fi 
-        #if [ -f libcudnn_cnn_train.so ];   then rm libcudnn_cnn_train.so;   fi 
-        #if [ -f libcudnn_cnn_infer.so ];   then rm libcudnn_cnn_infer.so;   fi 
-        #if [ -f libcudnn_adv_train.so ];   then rm libcudnn_adv_train.so;   fi 
-        #if [ -f libcudnn_ops_infer.so ];   then rm libcudnn_ops_infer.so;   fi 
-        #if [ -f libcudnn_adv_infer.so.8 ]; then rm libcudnn_adv_infer.so.8; fi 
-        #if [ -f libcudnn_ops_train.so.8 ]; then rm libcudnn_ops_train.so.8; fi 
-        #if [ -f libcudnn_cnn_train.so.8 ]; then rm libcudnn_cnn_train.so.8; fi 
-        #if [ -f libcudnn_cnn_infer.so.8 ]; then rm libcudnn_cnn_infer.so.8; fi 
-        #if [ -f libcudnn_adv_train.so.8 ]; then rm libcudnn_adv_train.so.8; fi 
-        #if [ -f libcudnn_ops_infer.so.8 ]; then rm libcudnn_ops_infer.so.8; fi 
-        #Logging "InstallcuDNN $infoStep3"
-        #ln libcudnn_adv_infer.so.$cuDNN_MajorVersion libcudnn_adv_infer.so.8
-        #ln libcudnn_ops_train.so.$cuDNN_MajorVersion libcudnn_ops_train.so.8
-        #ln libcudnn_cnn_train.so.$cuDNN_MajorVersion libcudnn_cnn_train.so.8
-        #ln libcudnn_cnn_infer.so.$cuDNN_MajorVersion libcudnn_cnn_infer.so.8
-        #ln libcudnn_adv_train.so.$cuDNN_MajorVersion libcudnn_adv_train.so.8
-        #ln libcudnn_ops_infer.so.$cuDNN_MajorVersion libcudnn_ops_infer.so.8
-        #ln libcudnn_adv_infer.so.8 libcudnn_adv_infer.so
-        #ln libcudnn_ops_train.so.8 libcudnn_ops_train.so
-        #ln libcudnn_cnn_train.so.8 libcudnn_cnn_train.so
-        #ln libcudnn_cnn_infer.so.8 libcudnn_cnn_infer.so
-        #ln libcudnn_adv_train.so.8 libcudnn_adv_train.so
-        #ln libcudnn_ops_infer.so.8 libcudnn_ops_infer.so
-        #ldconfig  
-        #source ~/.bashrc
+     #   Logging "InstallcuDNN $infoStep2"
+     #   cd  /usr/local/cuda-$CUDA_VERSION/targets/x86_64-linux/lib
+     #   if [ -f libcudnn_adv_infer.so ];   then rm libcudnn_adv_infer.so;   fi 
+      #  if [ -f libcudnn_ops_train.so ];   then rm libcudnn_ops_train.so;   fi 
+      #  if [ -f libcudnn_cnn_train.so ];   then rm libcudnn_cnn_train.so;   fi 
+     #   if [ -f libcudnn_cnn_infer.so ];   then rm libcudnn_cnn_infer.so;   fi 
+    #    if [ -f libcudnn_adv_train.so ];   then rm libcudnn_adv_train.so;   fi 
+    #    if [ -f libcudnn_ops_infer.so ];   then rm libcudnn_ops_infer.so;   fi 
+    #    if [ -f libcudnn_adv_infer.so.8 ]; then rm libcudnn_adv_infer.so.8; fi 
+    #    if [ -f libcudnn_ops_train.so.8 ]; then rm libcudnn_ops_train.so.8; fi 
+    #    if [ -f libcudnn_cnn_train.so.8 ]; then rm libcudnn_cnn_train.so.8; fi 
+    #    if [ -f libcudnn_cnn_infer.so.8 ]; then rm libcudnn_cnn_infer.so.8; fi 
+    #    if [ -f libcudnn_adv_train.so.8 ]; then rm libcudnn_adv_train.so.8; fi 
+    #    if [ -f libcudnn_ops_infer.so.8 ]; then rm libcudnn_ops_infer.so.8; fi 
+    #    Logging "InstallcuDNN $infoStep3"
+    #    ln libcudnn_adv_infer.so.$cuDNN_MajorVersion libcudnn_adv_infer.so.8
+    #    ln libcudnn_ops_train.so.$cuDNN_MajorVersion libcudnn_ops_train.so.8
+    #    ln libcudnn_cnn_train.so.$cuDNN_MajorVersion libcudnn_cnn_train.so.8
+    #    ln libcudnn_cnn_infer.so.$cuDNN_MajorVersion libcudnn_cnn_infer.so.8
+    #    ln libcudnn_adv_train.so.$cuDNN_MajorVersion libcudnn_adv_train.so.8
+    #    ln libcudnn_ops_infer.so.$cuDNN_MajorVersion libcudnn_ops_infer.so.8
+    #    ln libcudnn_adv_infer.so.8 libcudnn_adv_infer.so
+    #    ln libcudnn_ops_train.so.8 libcudnn_ops_train.so
+    #    ln libcudnn_cnn_train.so.8 libcudnn_cnn_train.so
+    #    ln libcudnn_cnn_infer.so.8 libcudnn_cnn_infer.so
+    #    ln libcudnn_adv_train.so.8 libcudnn_adv_train.so
+    #    ln libcudnn_ops_infer.so.8 libcudnn_ops_infer.so
+        ldconfig  
+        source ~/.bashrc
         Logging "InstallcuDNN $infoStepEnd"
         return 0                    
     }
@@ -745,6 +748,8 @@ Logging "#######################################################################
               -D HAVE_opencv_python3=ON \
               -D PYTHON_EXECUTABLE=/usr/bin/python3 \
               -D PYTHON2_EXECUTABLE=/usr/bin/python2 \
+              -D CUDNN_INCLUDE_DIR=/usr/local/cuda-$CUDA_VERSION/cudnn/include \
+              -D CUDNN_LIBRARY=/usr/local/cuda-$CUDA_VERSION/cudnn/lib \
               -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) \
               -D PYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
               -D PYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
