b12c4122dde678857084431ba7d0db9e3a4ce86d
wiegehtki.de <58086193+wiegehtki@users.noreply.github.com>
2022-02-26 10:46:08 +0100

    Update Final.sh

--- Final.sh
+++ Final.sh
@@ -391,15 +391,16 @@ Logging "#######################################################################
               echo " "
               echo $CUDA_Script $errorDownload
               return 1
-           fi   
-        else
-           apt -y install nvidia-cuda-toolkit
-           mkdir /usr/local/cuda
-           mkdir /usr/local/cuda/bin
-           ln -s /usr/local/cuda/bin/nvcc /usr/bin/nvcc
-           mkdir /usr/local/cuda-$CUDA_VERSION
-           ln -s /usr/local/cuda-$CUDA_VERSION /usr/local/cuda
-        fi
+           fi
+        fi   
+       # else
+       #    apt -y install nvidia-cuda-toolkit
+       #    mkdir /usr/local/cuda
+       #    mkdir /usr/local/cuda/bin
+       #    ln -s /usr/local/cuda/bin/nvcc /usr/bin/nvcc
+       #    mkdir /usr/local/cuda-$CUDA_VERSION
+       #    ln -s /usr/local/cuda-$CUDA_VERSION /usr/local/cuda
+       # fi
         
         cd ~/$CUDA_EXAMPLES_PATH/1_Utilities/deviceQuery
         make -j$(nproc) 
